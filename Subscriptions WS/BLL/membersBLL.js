aeae9e5fc04df416b4fa916f35583ca8850cb1cd
Boris Sverdlov <Borkya@gmail.com>
2023-06-06 00:44:01 +0300

    add membersBLL

---
+++ Subscriptions WS/BLL/membersBLL.js
@@ -0,0 +1,23 @@
+const membersWS = require('../DAL/membersWS');
+const Member = require('../models/Model');
+
+const fetchMemberData = async () => {
+    try {
+        const response = await membersWS.getAllMembers();
+        return response.data.map((member) => ({
+            id: member.id,
+            name: member.name,
+            email: member.email,
+            city: member.address.city,
+        }))
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+const storeMembersData = async () => {
+    const memberData = await fetchMemberData();
+    return Member.insertMany(memberData);
+};
+
+module.exports = { fetchMemberData, storeMembersData };
