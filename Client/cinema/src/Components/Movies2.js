b4f833442b9b3e5060a8210683ccbe4237aae0c8
Boris Sverdlov <Borkya@gmail.com>
2023-06-30 04:47:36 +0300

    Started the movie2 comp

---
+++ Client/cinema/src/Components/Movies2.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, Outlet, useLocation } from 'react-router-dom';
+import { Container } from '@mui/system';
+import { Button, TextField } from '@mui/material';
+
+const Movies2 = () => {
+    //TODO: finish this comp
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Movies2
